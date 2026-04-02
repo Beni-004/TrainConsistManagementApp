703991e6b078ed28290bccd972735d8eac3f8e89
Beni-004 <cuddlesbeniwal@gmail.com>
2026-04-02 09:13:58 +0530

    UC5

---
+++ src/UseCase5TrainConsistMngmt.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+
+public class UseCase5TrainConsistMngmt {
+    public static void main(String[] args) {
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
+
+        System.out.println("Final Train Formation: " + trainFormation);
+    }
+}
