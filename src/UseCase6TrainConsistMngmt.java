bc46194ca8446418ef6601cc01fdbd07bf93ccaf
Beni-004 <cuddlesbeniwal@gmail.com>
2026-04-02 09:15:58 +0530

    UC6

---
+++ src/UseCase6TrainConsistMngmt.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UseCase6TrainConsistMngmt {
+    public static void main(String[] args) {
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 78);
+        bogieCapacity.put("First Class", 24);
+
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
